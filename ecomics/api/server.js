41737a7e7f22f38cd2f6aea74a1c37d9237f1793
NazarenoCovian <nazareno.esso@hotmail.com>
2021-09-06 17:32:13 -0300

    agregue db.js

--- ecomics/api/server.js
+++ ecomics/api/server.js
@@ -1,1 +1,38 @@
 // server configs
+const express = require("express");
+const app = express();
+const volleyball = require("volleyball");
+const db = require("./db")
+// const routes = require("./routes")
+
+// logging middleware
+app.use(volleyball);
+
+app.use(express.static("build"))
+
+// parsing middleware
+app.use(express.json());
+// app.use("/api",routes)
+
+
+app.use("/api", (req, res) => {
+  res.sendStatus(404);
+});
+
+app.use((req, res) => {
+//   res.sendFile(__dirname + "/build/index.html");
+});
+// error middleware -> https://expressjs.com/es/guide/error-handling.html
+app.use((err, req, res, next) => {
+  console.log("ERROR");
+  console.log(err);
+  res.status(500).send(err.message);
+});
+db.sync({force:false})
+.then(function () {
+    // Recién ahora estamos seguros que la conexión fue exitosa
+    app.listen(3000, function () {
+        console.log('Server is listening on port 3001!');
+    });
+})
+.catch(console.error);
