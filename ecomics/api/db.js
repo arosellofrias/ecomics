5684b7ff7354fe0b9ab4365b5ab2c4fc91ccb498
ezevaleriano <ezequiel.valeriano@gmail.com>
2021-09-13 12:57:25 -0300

    cambios en rutas register

--- ecomics/api/db.js
+++ ecomics/api/db.js
@@ -1,13 +1,13 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('ecomics', "postgres", "lalalala.", {
+/* const db = new Sequelize('ecomics', "postgres", "lalalala.", {
   logging: false,
   host: 'localhost',
   dialect: 'postgres'
-});
-// const db = new Sequelize('ecomics', null, null, {
-//     logging: false,
-//     host: 'localhost',
-//     dialect: 'postgres'
-//   });
+}); */
+ const db = new Sequelize('ecomics', null, null, {
+     logging: false,
+     host: 'localhost',
+     dialect: 'postgres'
+   });
 
 module.exports = db
