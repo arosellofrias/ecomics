ac4564a3551e6f6069da97e80845d217741e590a
Augusto-Rosello <arosellofrias@gmail.com>
2021-09-13 12:29:20 -0300

    primerFuncionCarrito

--- ecomics/src/components/SingleComic.jsx
+++ ecomics/src/components/SingleComic.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import {singleComicRequest} from "../state/comics"
 import { useState } from "react";
+import axios from "axios";
 import styles from "./compStyles/singleComic.module.css"
 
 
@@ -13,10 +14,33 @@ export default (props)=>{
     console.log("PROPS",props)
     const params = useParams()
     const paramsId = params.id
+    const [counter,setCounter] = useState(0) 
+
+    const descreaseCounter = ()=>{
+       if(counter>0){setCounter(counter -1)}
+          
+      
+    }
+    const increaseCounter = ()=>{
+        if(counter< singleComic.stock){setCounter(counter +1)}
+    }
+    const handleSubmit = ()=>{
+        axios.post("http://localhost:3001/api/cart/add",{
+            cartId:isLoggedIn.id,
+            productId: singleComic.id,
+            cantidad: counter
+        })
+        .then(res=> res.data)
+        .catch(e=>console.log("ERRORSUBMIT",e))
+        
+    }
+    
      useEffect(()=>{
         dispatch(singleComicRequest(paramsId))
     },[paramsId])  
     const singleComic = useSelector((state) => state.singleComic);
+    const isLoggedIn = useSelector((state) => state.isLoggedIn);
+
     
     return(
         <div className={styles.container}>
@@ -28,7 +52,12 @@ export default (props)=>{
             <p className={styles.details}>{singleComic.descripcion}</p>
             <h2 className={styles.h1}>Precio:<strong>{singleComic.precio}</strong></h2>
             <h2 className={styles.h1}>Disponibles: <strong>{singleComic.stock}</strong></h2>
-            <button className={styles.h1}>Agregar al carrito</button>
+            <div>
+            <button className={styles.h1} onClick={handleSubmit}>Agregar al carrito</button>
+            <button onClick={descreaseCounter}>-</button>
+            <button onClick={increaseCounter}>+</button>
+            <strong>{counter}</strong>
+            </div>
             </div>
             </>
             : <h1>Loading..</h1>}
