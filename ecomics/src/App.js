811da2e4e951da5b564d1379728ae79f1f76e0cc
lautaro1451 <manfiolautaro@gmail.com>
2021-09-13 19:13:20 -0300

    categorias navbar y creacion componentes

--- ecomics/src/App.js
+++ ecomics/src/App.js
@@ -5,15 +5,15 @@ import { useDispatch, useSelector } from "react-redux";
 import { getComicsRequest } from "./state/comics";
 import Comics from "./components/Comics";
 import SingleComic from "./components/SingleComic";
-import Search from "./components/Search"
-import Carrito from "./components/Carrito"
-
+import Search from "./components/Search";
+import Carrito from "./components/Carrito";
+import MangasFilter from "./components/MangasFilter";
+import ComicsFilter from "./components/ComicsFilter";
 import LoginUser from "./components/LoginUser";
 import { Logout } from "./components/Logout";
 
 import Register from "./components/Register";
 import Navbar from "./components/Navbar";
-
 
 function App() {
   const dispatch = useDispatch();
@@ -30,26 +30,26 @@ function App() {
     dispatch(getComicsRequest(comic));
   };
  */
-  useEffect(()=>{
-    dispatch(getComicsRequest(),[])
-  }) 
-  
+  useEffect(() => {
+    dispatch(getComicsRequest(), []);
+  });
+
   return (
     <div className="App">
-      <Navbar/>
-      {console.log("ISLOGGEDIN===>", isLoggedIn )}
+      <Navbar />
+      {console.log("ISLOGGEDIN===>", isLoggedIn)}
       <Switch>
         {/* <Route path="/search" component={Search}/> */}
         <Route exact path="/comics" component={Comics} />
         <Route path="/comics/:id" component={SingleComic} />
         <Route path="/login" component={LoginUser} />
         <Route path="/logout" component={Logout} />
-        <Route path="/cart" component={Carrito}/>
-        <Route path ="/register" component={Register}/>
-
+        <Route path="/cart" component={Carrito} />
+        <Route path="/register" component={Register} />
+        <Route path="/category/mangas" component={MangasFilter} />
+        <Route path="/category/comics" component={ComicsFilter} />
         <Redirect to="/comics" />
       </Switch>
-
     </div>
   );
 }
---
+++ ecomics/src/components/ComicsFilter.jsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getComicsRequest } from "../state/comics";
+
+export default () => {
+  return <h1>COMICS</h1>;
+};
